710d34ff555506040fd2fb327b9e043997c2dc8a
DennisPulles <dennispulles@hotmail.com>
2020-12-30 14:06:16 +0100

    Updated sql package
    Placed query builders in sqlmanager
    Placed connection info in sqlmanager
    Placed sql executor in sqlmanager
    
    deleted the sqlselect class

--- Java/DB/SqlManager.java
+++ Java/DB/SqlManager.java
@@ -1,5 +1,77 @@
 package Java.DB;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class SqlManager {
     
+    //IMPORTANT CONNECTION INFO
+
+    private String connectionUrl = "jdbc:sqlserver://localhost;databaseName=Quatro;integratedSecurity=true;";
+
+    // Connection manages information about the connection with the database.
+    Connection con = null;
+
+    // Statement allows us to execute SQL queries
+    Statement stmt = null;
+
+    // ResultSet is the table that the database returns.
+    ResultSet rs = null;
+
+    private String type;
+
+    private String query;
+
+    private ResultSet select(){
+        createSelectSql(type);
+        executeSql(query);
+        return rs;
+    }
+
+    
+    private String createSelectSql(String type){
+        query = "SELECT * FROM " + type;
+        return query;
+    }
+
+    private String createInsertSql(String type){
+        query = "INSERT INTO " + type + "()";
+    }
+
+    // executing the sql on the database with the allready created query
+    private ResultSet executeSql(String query){
+
+        try {
+            // 'Import' the driver you downloaded
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            // Create connection with the database.
+            con = DriverManager.getConnection(connectionUrl);
+            
+            stmt = con.createStatement();
+            // Execute query on the database.
+            rs = stmt.executeQuery(query);
+        }
+        // Handle any errors that may have occurred.
+        catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (rs != null)
+                try {
+                    rs.close();
+                } catch (Exception e) {
+                }
+            if (stmt != null)
+                try {
+                    stmt.close();
+                } catch (Exception e) {
+                }
+            if (con != null)
+                try {
+                    con.close();
+                } catch (Exception e) {
+                }
+        }
+        return rs;
+    }
 }
