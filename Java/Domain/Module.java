2b8a1f7604d01f7f1b759b511208590f78ebdc2e
TimdeLaater <tdelaater@gmail.com>
2021-01-07 12:51:33 +0100

    Module,Webcast & ContentItem
    .

--- Java/Domain/Module.java
+++ Java/Domain/Module.java
@@ -1,5 +1,14 @@
 package Java.Domain;
 
-public class Module {
-    
+public class Module extends ContentItems{
+    private String title;
+    private String version;
+    private int number;
+    private String description;
+    private String contactName;
+    private String contactEmail;
+
+    public Module(){
+        
+    }
 }
--- Java/Domain/Webcast.java
+++ Java/Domain/Webcast.java
@@ -1,5 +1,15 @@
 package Java.Domain;
 
-public class Webcast {
-    
+public class Webcast extends ContentItems{
+    private String tile;
+    private int timeSpan;
+    private String description;
+    private String speakerName;
+    private String speakerCompany;
+    private String url;
+    private int amountWatched;
+
+    public Webcast(){
+        
+    }
 }
