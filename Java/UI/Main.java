96026fce27c1520973ed8efa05546c2e15710ce5
Guus Korbijn <guus.korbijn@gmail.com>
2021-01-07 12:55:42 +0100

    Buttons
    Added buttons

--- Java/UI/Main.java
+++ Java/UI/Main.java
@@ -1,66 +1,91 @@
 package Java.UI;
 
-import javafx.application.Application; 
-import javafx.scene.Scene; 
-import javafx.scene.layout.*; 
-import javafx.scene.control.*; 
-import javafx.stage.Stage; 
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.layout.*;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.scene.control.*;
 
-public class TestFXQuatro extends Application { 
+import javafx.stage.Stage;
+
+public class Main extends Application { 
     public void start(Stage primaryStage) 
-    { 
+    {
         primaryStage.setTitle("Codecademy"); 
-  
-        Menu data = new Menu("Data");
-        Menu nieuw = new Menu("Nieuw");
-        Menu wijzigen = new Menu("Wijzigen");
-        Menu rapporten = new Menu("Rapport");
 
-        // data
+        Menu data = new Menu("Data");
+        Menu add = new Menu("Nieuw");
+        Menu modify = new Menu("Wijzigen");
+        Menu overviews = new Menu("Rapport");
+
+        // Data menu items
         MenuItem cursussen = new MenuItem("Cursussen");
         MenuItem cursisten = new MenuItem("Cursisten");
         MenuItem modules = new MenuItem("Modules");
         
-        //nieuw
-        MenuItem cursus = new MenuItem("Cursus");
+        // New menu items
+        MenuItem course = new MenuItem("Cursus");
         MenuItem module = new MenuItem("Module");
         MenuItem webcast = new MenuItem("Webcast");
 
-        // rapporten
-        MenuItem overzicht1 = new MenuItem("Overzicht 1");
-        MenuItem overzicht2 = new MenuItem("Overzicht 2");
-        MenuItem overzicht3 = new MenuItem("Overzicht 3");
-        MenuItem overzicht4 = new MenuItem("Overzicht 4");
-        MenuItem overzicht5 = new MenuItem("Overzicht 5");
-        MenuItem overzicht6 = new MenuItem("Overzicht 6");
+        // Overview menu items
+        MenuItem overview1 = new MenuItem("Overzicht 1");
+        MenuItem overview2 = new MenuItem("Overzicht 2");
+        MenuItem overview3 = new MenuItem("Overzicht 3");
+        MenuItem overview4 = new MenuItem("Overzicht 4");
+        MenuItem overview5 = new MenuItem("Overzicht 5");
+        MenuItem overview6 = new MenuItem("Overzicht 6");
 
         data.getItems().add(cursussen); 
         data.getItems().add(cursisten); 
         data.getItems().add(modules); 
-        nieuw.getItems().add(cursus);
-        nieuw.getItems().add(module);
-        nieuw.getItems().add(webcast);
-        wijzigen.getItems().add(cursus);
-        wijzigen.getItems().add(module);
-        wijzigen.getItems().add(webcast);
-        rapporten.getItems().add(overzicht1);
-        rapporten.getItems().add(overzicht2);
-        rapporten.getItems().add(overzicht3);
-        rapporten.getItems().add(overzicht4);
-        rapporten.getItems().add(overzicht5);
-        rapporten.getItems().add(overzicht6);
+        add.getItems().add(course);
+        add.getItems().add(module);
+        add.getItems().add(webcast);
+        modify.getItems().add(course);
+        modify.getItems().add(module);
+        modify.getItems().add(webcast);
+        overviews.getItems().add(overview1);
+        overviews.getItems().add(overview2);
+        overviews.getItems().add(overview3);
+        overviews.getItems().add(overview4);
+        overviews.getItems().add(overview5);
+        overviews.getItems().add(overview6);
   
         MenuBar menuBar = new MenuBar(); 
   
         menuBar.getMenus().add(data); 
-        menuBar.getMenus().add(nieuw); 
-        menuBar.getMenus().add(wijzigen); 
-        menuBar.getMenus().add(rapporten); 
+        menuBar.getMenus().add(add); 
+        menuBar.getMenus().add(modify); 
+        menuBar.getMenus().add(overviews); 
 
-        VBox vb = new VBox(menuBar); 
+        // Welcome text (not in de application yet)
+        Text welcomeText = new Text("Welkom");
+        
+        // Buttons 
+        Button courses = new Button("Cursussen");
+        Button students = new Button("Cursisten");
+        Button certificates = new Button("Certificaten");
+        Button registrations = new Button("Registraties");
   
-        Scene sc = new Scene(vb, 960, 600); 
-  
+        HBox hboxButtons = new HBox();
+
+        hboxButtons.getChildren().add(courses);
+        hboxButtons.getChildren().add(students);
+        hboxButtons.getChildren().add(certificates);
+        hboxButtons.getChildren().add(registrations);
+        hboxButtons.setPadding(new Insets(200, 100, 200, 0));
+        hboxButtons.setSpacing(80.0);
+
+        BorderPane layout = new BorderPane();
+        layout.setTop(menuBar);
+        layout.setCenter(hboxButtons);
+
+        Scene sc = new Scene(layout, 960, 600); 
+
         primaryStage.setScene(sc); 
         primaryStage.setMaximized(true);
         primaryStage.show(); 
@@ -71,4 +96,3 @@ public class TestFXQuatro extends Application {
         launch(args); 
     } 
 } 
-
