aac9a6f257d10e865d49bcaf577558aac22d90a6
TimdeLaater <tdelaater@gmail.com>
2021-01-21 14:42:29 +0100

    Home page completed and CourseScene class made

---
+++ Java/UI/CourseScene.java
@@ -0,0 +1,14 @@
+package Java.UI;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class CourseScene extends Application {
+
+    @Override
+    public void start(Stage courseStage) throws Exception {
+        
+
+    }
+    
+}
