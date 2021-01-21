d8120dd2e303649d1660fea68a32962f6c2a1b0f
DennisPulles <dennispulles@hotmail.com>
2021-01-21 13:52:13 +0100

    RegistrationSql functional, ContentItem publicationdat is now GETDATE()

--- Java/DB/ContentItemSql.java
+++ Java/DB/ContentItemSql.java
@@ -4,7 +4,7 @@ public class ContentItemSql {
     
     //Create
     private String insertContentItemSql(String input[]){
-        return "INSERT INTO ContentItem(Status, PublicationDate, Title, Discription) VALUES ('" + input[0] + "', '" + input[1] + "', '" + input[2] + "', '" + input[3] + "')";
+        return "INSERT INTO ContentItem(Status, PublicationDate, Title, Discription) VALUES ('" + input[0] + "', GETDATE(), '" + input[2] + "', '" + input[3] + "')";
     }
 
     //Update
