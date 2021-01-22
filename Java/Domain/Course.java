6cb5c07fef8445050494fa6093cddc557b9a2df5
DennisPulles <dennispulles@hotmail.com>
2021-01-22 10:23:14 +0100

    removed sql from manager and re-added variables to 4 domain classes
    removed sql from the manager
    added the basic variables to contentitems course module and webasts.

--- Java/Domain/Course.java
+++ Java/Domain/Course.java
@@ -1,5 +1,19 @@
-package Java.Domain;
-
-public class Course {
-    
-}
+package Java.Domain; 
+ 
+import java.util.ArrayList; 
+ 
+public class Course { 
+    private String name; 
+    private String subject; 
+    private String introductoryText; 
+    private String difficulty; 
+    private ArrayList<Module> modules; 
+    private ArrayList<Course> recommendedCourses; 
+    private ArrayList<Student> student; 
+ 
+    public Course(String name, String subject, String introductoryText, String difficulty, ArrayList<Module> modules, 
+            ArrayList<Course> recommendedCourses, ArrayList<Student> student) { 
+ 
+    } 
+ 
+} 
