58115ebf4f335d0749176a622d09349a3b87fcb9
Wouter Zegers <zegerswouter2@gmail.com>
2021-01-24 15:58:29 +0100

    course domain

--- Java/Domain/Course.java
+++ Java/Domain/Course.java
@@ -1,25 +1,83 @@
-package Java.Domain; 
+package Java.Domain;
 
+import java.sql.Date;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
-import java.sql.ResultSet;
-import Java.DB.*; 
+import Java.DB.SqlManager;
+import Java.DB.CourseSql;
  
 public class Course { 
     private String name; 
     private String subject; 
     private String introductoryText; 
     private String difficulty; 
-    private ArrayList<Module> modules; 
-    private ArrayList<Course> recommendedCourses; 
-    private ArrayList<Student> student; 
+    protected ArrayList<Course> courseInfoArrayList;
     
     SqlManager manager = new SqlManager();
     CourseSql courseSql = new CourseSql();
 
     ResultSet coursesRS = manager.executeSql(courseSql.selectCoursesSql());
-    
-    public Course(String name, String subject, String introductoryText, String difficulty, ArrayList<Module> modules, ArrayList<Course> recommendedCourses, ArrayList<Student> student) { 
-        
+
+    public Course(String name, String subject, String introductoryText, String difficulty) {
+        this.courseInfoArrayList = new ArrayList<>();
+        this.name = name;
+        this.subject = subject;
+        this.introductoryText = introductoryText;
+        this.difficulty = difficulty;
     }
+
+    public void getCourseResult() {
+        ResultSet courseRS = manager.executeSql(courseSql.selectCoursesSql());
+        try {
+            while (courseRS.next()) {
+                Course course = new Course(courseRS.getString("CourseName"), courseRS.getString("Subject"), courseRS.getString("IntroductoryText"), courseRS.getString("Difficulty"));
+                courseInfoArrayList.add(course);
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public String getCourseName() {
+        return name;
+    }
+
+    public void setCourseName(String name) {
+        this.name = name;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getIntroductoryText() {
+        return introductoryText;
+    }
+
+    public void setIntroductoryText(String introductoryText) {
+        this.introductoryText = introductoryText;
+    }
+
+    public String getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(String difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public ArrayList<Course> getCourseInfoArrayList() {
+        return courseInfoArrayList;
+    }
+
+    public void setCourseInfoArrayList(ArrayList<Course> courseInfoArrayList) {
+        this.courseInfoArrayList = courseInfoArrayList;
+    }
+   
 } 
