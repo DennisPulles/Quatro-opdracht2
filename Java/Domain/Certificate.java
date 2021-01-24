bb86d962ae01485f0c7c667401da268f26bb826e
DennisPulles <dennispulles@hotmail.com>
2021-01-24 20:55:00 +0100

    Domain/Certificate made crud functional

--- Java/Domain/Certificate.java
+++ Java/Domain/Certificate.java
@@ -1,5 +1,61 @@
 package Java.Domain;
 
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+import Java.DB.SqlManager;
+import Java.DB.CertificateSql;
+
 public class Certificate {
+    private int certificateID;
+    private float grade;
+    private String signatoryName;
+    protected ArrayList<Certificate> certificateInfo;
+
+    public Certificate(int certificateID, float grade, String signatoryName){
+        this.certificateInfo = new ArrayList<>();
+        this.certificateID = certificateID;
+        this.grade = grade;
+        this.signatoryName = signatoryName;
+    }
+
+    //give this class access to the functions in SqlManager and CertificateSql
+    SqlManager manager = new SqlManager();
+    CertificateSql certificateSql = new CertificateSql();
     
+    public void getModuleResult() {
+        ResultSet certificateRS = manager.executeSql(certificateSql.selectCertificateInRegistrationSql());
+        try {
+            while (certificateRS.next()) {
+                Certificate certificate = new Certificate(certificateRS.getInt("certificateID"), certificateRS.getFloat("grade"), certificateRS.getString("signatoryName"));
+                certificateInfo.add(certificate);
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public void updateCertificate(String ID, String input[]){
+        manager.executeSql(certificateSql.updateCertificateSql(ID, input));
+    }
+
+    public void insertCertificate(String input[]){
+        manager.executeSql(certificateSql.insertCertificateSql(input));
+    }
+
+    public void deleteCertificate(String ID){
+        manager.executeSql(certificateSql.deleteCertificateSql(ID));
+    }
+
+    public int getCertificateID(){
+        return certificateID;
+    }
+
+    public float getGrade(){
+        return grade;
+    }
+
+    public String getSignatoryName(){
+        return signatoryName;
+    }
 }
