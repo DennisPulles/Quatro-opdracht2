a614a5d0728e377cf2579ebb67f353aff39b31c5
DennisPulles <dennispulles@hotmail.com>
2021-01-24 17:10:50 +0100

    Domain/module updated
    number is now SerialNumber, a constructor has been added and the function to create all modules has been added.

--- Java/Domain/Module.java
+++ Java/Domain/Module.java
@@ -1,14 +1,42 @@
 package Java.Domain; 
- 
+
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+import Java.DB.SqlManager;
+import Java.DB.ModuleSql;
+
 public class Module extends ContentItems{ 
     private String title; 
     private String version; 
-    private int number; 
+    private int serialNumber; 
     private String description; 
     private String contactName; 
     private String contactEmail; 
  
-    public Module(){ 
-         
-    } 
+    public Module(String title, String version, int serialNumber, String description, String contactName, String contactEmail){ 
+        this.title = title;
+        this.version = version;
+        this.serialNumber = serialNumber;
+        this.description = description;
+        this.contactName = contactName;
+        this.contactEmail = contactEmail;
+    }
+
+    //give this class access to the functions in SqlManager and ModuleSql
+    SqlManager manager = new SqlManager();
+    ModuleSql moduleSql = new ModuleSql();
+
+    //get an arraylist of all Module in the database
+    public void getModuleResult() {
+        ResultSet moduleRS = manager.executeSql(moduleSql.selectModulesSql());
+        try {
+            while (moduleRS.next()) {
+                //create new Module module
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
 } 
