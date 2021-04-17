e8e938659f2b3579c098b0d1d56488ac3dff2fe9
DennisPulles <dennispulles@hotmail.com>
2021-04-17 14:30:02 +0200

    DB/CertificateSql added get unfinished certificates

--- Java/DB/CertificateSql.java
+++ Java/DB/CertificateSql.java
@@ -11,7 +11,7 @@ public class CertificateSql {
     public String selectCertificateInRegistrationSql(){
         return "SELECT * FROM Certificate INNER JOIN Registration ON Certificate.CertificateID = Registration.CertificateID";
     }
-
+    
     //Courses for which a student has a certificate
     public String selectCertificatesEarnedSql(String email){
         return "SELECT Registration.CourseName FROM Student INNER JOIN Registration ON Student.StudentEmail = Registration.StudentEmail INNER JOIN Certificate ON Registration.CertificateID = Certificate.CertificateID WHERE Student.StudentEmail = '" + email + "' AND SignatoryName IS NOT NULL";
@@ -20,6 +20,11 @@ public class CertificateSql {
     //Certificate from specific student.
     public String selectCertificateSpecificStudent(String ID){
         return "SELECT * FROM Certificate INNER JOIN Registration ON Certificate.CertificateID = Registration.CertificateID WHERE Registration.StudentEmail = '" + ID + "'";
+    }
+
+    //Certificates that have a student but no signatory name
+    public String selectUnfinishedCertificates(){
+        return "SELECT * FROM Certificate INNER JOIN Registration ON Certificate.CertificateID = Registration.CertificateID WHERE SignatoryName IS NULL";
     }
 
     //update
